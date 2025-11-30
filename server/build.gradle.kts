53e04773c3ad0b596c21f45ab6b28ee4f35cd2bb
T45K <tasktas9@gmail.com>
2025-11-30 13:36:12 +0900

    add koog-google-search-tool dependency to server

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -7,6 +7,9 @@ dependencies {
     implementation(libs.koog.a2a.server)
     implementation(libs.koog.a2a.transport.server)
 
+    // https://github.com/T45K/koog-google-search-tool/releases/tag/0.0.1
+    implementation(files("../deps/koog-google-search-tool-0.0.1.jar"))
+
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.cio)
